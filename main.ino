fb22cd2b98fb2bd7064d49dc3087fcffd1891f42
alcex <thealcex@gmail.com>
2019-08-10 13:16:10 -0400

    :memo: Redraw clock

--- main.ino
+++ main.ino
@@ -12,22 +12,27 @@ const int PIN_BL = 10;  // Backlight pin.
 
 LiquidCrystal lcd(PIN_RS,  PIN_EN,  PIN_D4,  PIN_D5,  PIN_D6,  PIN_D7);
 
+// Draws the 6 character clock icon at a specified (x, y) position.
+void drawClock(int x=0, int y=0) {
+    for (int j = 0; j < 2; j++) {
+        for (int i = 0; i < 3; i++) {
+            lcd.setCursor(x+i, y+j);
+            lcd.write(i + (j == 1 ? 3:0));  //If drawing bottom row start at 3, if not, start at 0.
+        }
+    }   
+}
+
 void setup() {
  lcd.begin(16, 2);
  
  lcd.createChar(0, CLOCK_TOP_LEFT);
- lcd.createChar(1, CLOCK_TOP_RIGHT);
- lcd.createChar(2, CLOCK_BOTTOM_LEFT);
- lcd.createChar(3, CLOCK_BOTTOM_RIGHT);
- 
- lcd.home();
- lcd.write(byte(0));
- lcd.setCursor(1, 0);
- lcd.write(1);
- lcd.setCursor(0, 1);
- lcd.write(2);
- lcd.setCursor(1, 1);
- lcd.write(3);
+ lcd.createChar(1, CLOCK_TOP_MIDDLE);
+ lcd.createChar(2, CLOCK_TOP_RIGHT);
+ lcd.createChar(3, CLOCK_BOTTOM_LEFT);
+ lcd.createChar(4, CLOCK_BOTTOM_MIDDLE);
+ lcd.createChar(5, CLOCK_BOTTOM_RIGHT);
+
+ drawClock();
 
  pinMode(PIN_BL, OUTPUT);
  digitalWrite(PIN_BL, HIGH);
@@ -36,3 +41,4 @@ void setup() {
 void loop() {
 
 }
+
